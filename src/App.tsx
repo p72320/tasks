e1418ea32299f6b3db584a338687fc6dee535b67
andrepa <pbabik@udel.edu>
2024-02-13 22:50:20 -0500

    Deleted TEST

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Andre Babik was here</p>
-            <p>TEST</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
